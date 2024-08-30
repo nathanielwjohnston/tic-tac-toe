24c05996e679d74427dcfeed324507fa53c44473
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2024-08-30 21:22:41 +0100

    Add basic player creation

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ const gameBoard = (function () {
 
   return {updateBoard, getBoard};
 })();
+
+function createPlayer (marker) {
+  // Would maybe update this to include some sort of error handling in future
+  if (marker !== "X" && marker !== "O") {
+    console.log("Unsupported marker");
+    return;
+  }
+
+  let score = 0;
+
+  const getScore = () => score;
+  // addScore also returns the updated score - obviously shouldn't be used
+  // just for getting score
+  const addScore = () => score++;
+
+  return {marker, getScore, addScore};
+}
